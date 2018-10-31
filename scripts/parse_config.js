489d4b02988901bacbab6b8305c062d6142c12b9
samgomena <samiam4612@gmail.com>
2018-10-31 00:18:42 -0700

    feat: Minor updates and error checking.

--- scripts/parse_config.js
+++ scripts/parse_config.js
@@ -12,6 +12,7 @@ Pretty Rock
 #####################
 12,12,1,1,None
 13,12,0,1,Tree
+15,13,0,4,Royal Diamonds
 14,12,0,2,None`;
 
 const TERRAIN_MAP = {
@@ -25,14 +26,14 @@ const TERRAIN_MAP = {
 
 const NUM_REGEX = /(\d+)/;
 const COORD_REGEX = /(\d+),\s*(\d+)/;
-const MAP_ITEM_REGEX = /(\d+),\s*(\d+),(\d+),(\d+),(\w+)/;
+const MAP_ITEM_REGEX = /(\d+),\s*(\d+),\s*(\d+),\s*(\d+),\s*([\w\s]+)/;
 
-export function parse(game_config) {
+function parse(game_config) {
     const GAME = {};
     GAME.map = {};
+    GAME.map.objects = [];
     GAME.hero = {};
-    GAME.hero.position = {};
-    GAME.map.map_objects = [];
+    GAME.hero.tools = {};
 
     let split_map_file = game_config.split("\n");
 
@@ -40,10 +41,7 @@ export function parse(game_config) {
     let [game_title, board_size, first_delimiter] = split_map_file.splice(0, 3);
 
     GAME.title = game_title;
-    GAME.map.size = {
-        width: board_size,
-        height: board_size,
-    };
+    GAME.map.width = GAME.map.height = +board_size;
 
     let delimiter = first_delimiter.charAt(0);
 
@@ -52,18 +50,16 @@ export function parse(game_config) {
 
     // Destruct regex match groups into x, y coordinate variables
     let [, hero_start_x, hero_start_y] = split_map_file.splice(0, 1)[0].match(COORD_REGEX);
-    GAME.hero.x = hero_start_x;
-    GAME.hero.y = hero_start_y;
+    GAME.hero.pos = {
+        x: +hero_start_x,
+        y: +hero_start_y,
+    };
 
     let [, hero_energy] = split_map_file.splice(0, 1)[0].match(NUM_REGEX);
-    GAME.hero.energy = hero_energy;
+    GAME.hero.energy = +hero_energy;
 
     let [, hero_whiffles] = split_map_file.splice(0, 1)[0].match(NUM_REGEX);
-    GAME.hero.whiffles = hero_whiffles;
-
-    // GAME.hero.tools = new Proxy({}, {get: (target, name) => (++target[name] || 1)});
-
-    GAME.hero.tools = {};
+    GAME.hero.whiffles = +hero_whiffles;
 
     // Loop until we see the delimiter again
     while (!split_map_file[0].match(delimiter_regex)) {
@@ -75,35 +71,43 @@ export function parse(game_config) {
     // Remove closing delimiter
     split_map_file.splice(0, 1);
 
-
-
     // Loop through everything after the delimiter
     split_map_file.forEach(map_item => {
 
         let [, x, y, visibility, terrain, name] = map_item.match(MAP_ITEM_REGEX) || [];
 
         if (x > board_size || y > board_size) {
-            throw Error(`ERROR: [${x}, ${y}] out of range [${board_size}, ${board_size}].`);
-        } else if (!TERRAIN_MAP[terrain]) {
-            throw Error(`ERROR: ${terrain} is not a valid terrain value.`)
+            throw Error(`Position of ${name} at (${x}, ${y}) is out of bounds.`);
         }
 
-        GAME.map_objects.push({
-            x: parseInt(x),
-            y: parseInt(y),
-            visible: Boolean(parseInt(visibility)),
+        if (!TERRAIN_MAP[terrain]) {
+            throw Error(`${terrain} is not a valid terrain value.`)
+        }
+
+        GAME.map.objects.push({
+            x: +x,
+            y: +y,
+            visible: Boolean(+visibility),
             terrain: TERRAIN_MAP[terrain],
             name: name === 'None' ? "" : name,
         });
     });
 
-    console.log(GAME.map_objects);
-    console.log(game_title, board_size);
 
-    let contains_diamonds = Boolean(GAME.map_objects.filter(board_object => {
-        return board_object.name === "Royal Diamonds"
+    // Perform checks
+    if(GAME.hero.pos.x > GAME.map.width || GAME.hero.pos.y > GAME.map.height) {
+        throw Error(`Starting position of (${GAME.hero.pos.x}, ${GAME.hero.pos.y}) is out of bounds.`);
+    }
+
+    let contains_diamonds = Boolean(GAME.map.objects.filter(board_object => {
+        return board_object.name === "Royal Diamonds";
     }).length);
-    console.log(contains_diamonds);
+
+    if(!contains_diamonds) {
+        throw Error(`The map does not contain the royal diamonds.`);
+    }
 
     return GAME;
 }
+
+console.log(parse(map_file));
