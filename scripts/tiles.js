18ec29f909d916a3e93781eb72490f2a44e978ae
Aaron <aachansy2007@gmail.com>
2018-10-31 21:22:38 -0700

    Remove inheritance
    Inheritance is unnecessary.

--- scripts/tiles.js
+++ scripts/tiles.js
@@ -3,62 +3,12 @@
 // and javascript will be ok with it. The could be the 
 // datatype used for our map.
 
-class Tile {
-  constructor(obstacle, color, cost, hidden) { 
+export default class Tile {
+  constructor(terrain, obstacle, color, cost, hidden) { 
+    this.terrain = terrain;
     this.obstacle = obstacle;
     this.color = color;
     this.cost = cost;
     this.hidden = hidden;
-  }
-  printInfo() {
-    return `Obstacle: ${this.obstacle}\n`
-            +`Color: ${this.color}`;
-  }
-}
-
-// Eventually getEvent() will be modified to return special items or messages to the player.
-export class Meadow extends Tile {
-  constructor(obstacle) {
-    super(obstacle, [0, 255, 0], 1, true);
-  }
-  getEvent() {
-    return this.cost;
-  }
-}
-
-export class Water extends Tile {
-  constructor(obstacle) {
-    // FIXME: -1 may be a bit confusing.
-    super(obstacle, [0, 0, 255], -1, true);
-  }
-  getEvent() {
-    return this.cost;
-  }
-}
-
-export class Swamp extends Tile {
-  constructor(obstacle) {
-    super(obstacle, [90, 90, 0], 4, true);
-  }
-  getEvent() {
-    return this.cost;
-  }
-}
-
-export class Bog extends Tile {
-  constructor(obstacle) {
-    super(obstacle, [90, 90, 0], 3, true);
-  }
-  getEvent() {
-    return this.cost;
-  }
-}
-
-export class Forest extends Tile {
-  constructor(obstacle) {
-    super(obstacle, [152, 251, 152], 2, true);
-  }
-  getEvent() {
-    return this.cost;
   }
 }
