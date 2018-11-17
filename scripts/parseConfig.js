b3236060f6e017a4a37c05773a6e56caf83509e2
Aaron <aachansy2007@gmail.com>
2018-11-17 14:14:43 -0800

    Add power bar for testing
    Begin power bar implementation

--- scripts/parseConfig.js
+++ scripts/parseConfig.js
@@ -23,8 +23,9 @@ Pretty Rock
 12, 12, 1, 1, None
 13, 12, 0, 1, Tree
 15, 13, 0, 6, Royal Diamonds
-11, 11, 0, 0, Binoculars
+11, 11, 0, 1, Binoculars
 15, 16, 0, 4, Bog 
+5, 5, 1, 0, Power Bar
 14, 12, 0, 2, None`;
 
 
