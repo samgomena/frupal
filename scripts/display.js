76145d1ac4c08cfe9c550e902146527b58dc0ba1
Lauren Braun <lbraun21@gmail.com>
2018-11-25 20:23:26 -0800

    we don't need to see items on the display..that's for inventory

--- scripts/display.js
+++ scripts/display.js
@@ -3,6 +3,7 @@ class Display {
     // TODO: Make display render inside the game display (Upper right corner).
     this.map = map;
     this.hero = person;
+    this.inv_display = document.getElementById("invDisplay");
     this.displayEl = document.getElementById("display");
     // Location, Energy, Money nodes.
     if(this.displayEl.innerHTML == "") {
@@ -19,7 +20,7 @@ class Display {
     const money = this.hero.getMoney();
     const energy = this.hero.getEnergy();
     let item = this.hero.getPlayerLocItem();
-    const inventory = this.hero.getPlayerInventory();
+    //const inventory = this.hero.getPlayerInventory();
 
     const locationText = `Current Location: (${location.x + 1}, ${location.y + 1})`;
     const moneyText = `Whiffles: ${money}`;
@@ -28,16 +29,16 @@ class Display {
     if (item !== "None") {
       item = item.name;
     }
-    const itemText = `Item: ${item}`;
-    const invText = `Inventory: ${inventory.toString()}`;
+    //const itemText = `Item: ${item}`;
+    //const invText = `Inventory: ${inventory.toString()}`;
 
     try {
       this.lNode.replaceData(0, 50, locationText);
       this.eNode.replaceData(0, 50, energyText);
       this.mNode.replaceData(0, 50, moneyText);
       this.tNode.replaceData(0, 50, terrainText);
-      this.iNode.replaceData(0, 50, itemText);
-      this.inNode.replaceData(0, 50, invText);
+      //this.iNode.replaceData(0, 50, itemText);
+      //this.inNode.replaceData(0, 50, invText);
     } catch (err) {
       // Makes sure duplicates are not created during HMR
       this.displayEl.innerHTML = "";
@@ -48,16 +49,22 @@ class Display {
     return true;
   }
 
+  createInvNode() {
+    const inventory = this.hero.getPlayerInventory();
+    this.inNode = document.createTextNode(`Inventory: ${inventory.toString()}`);
+    this.displayEl.appendChild(this.inNode);
+  }
+
   createNodes() {
     const terrain = this.hero.getPlayerLocInfo();
     const item = this.hero.getPlayerLocItem();
-    const inventory = this.hero.getPlayerInventory(); 
+    //const inventory = this.hero.getPlayerInventory();
     this.lNode = document.createTextNode(`Current Location: (${this.hero.x}, ${this.hero.y})`);
     this.eNode = document.createTextNode("Energy: 100");
     this.mNode = document.createTextNode("Whiffles: 100");
     this.tNode = document.createTextNode(`Terrain: ${terrain}`);
-    this.iNode = document.createTextNode(`Item: ${item}`);
-    this.inNode = document.createTextNode(`Inventory: ${inventory.toString()}`);
+    //this.iNode = document.createTextNode(`Item: ${item}`);
+    //this.inNode = document.createTextNode(`Inventory: ${inventory.toString()}`);
 
     this.displayEl.appendChild(this.lNode);
     this.displayEl.appendChild(document.createElement("br"));
@@ -66,10 +73,10 @@ class Display {
     this.displayEl.appendChild(this.mNode);
     this.displayEl.appendChild(document.createElement("br"));
     this.displayEl.appendChild(this.tNode);
-    this.displayEl.appendChild(document.createElement("br"));
-    this.displayEl.appendChild(this.iNode);
-    this.displayEl.appendChild(document.createElement("br"));
-    this.displayEl.appendChild(this.inNode);
+    //this.displayEl.appendChild(document.createElement("br"));
+    //this.displayEl.appendChild(this.iNode);
+    //this.displayEl.appendChild(document.createElement("br"));
+    //this.displayEl.appendChild(this.inNode);
 
   }
 }
